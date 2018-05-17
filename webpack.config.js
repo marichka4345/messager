0666a6780143defd648c6c0e1cc3a2727e4a9788
mtretiak <mtretiak@corelogic.com>
2018-05-17 15:06:02 +0300

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPluginConfig = new ExtractTextPlugin({
+  filename: 'main.css',
+  allChunks: true
+});
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+  resolve: { extensions: ['.js'] },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.global\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /^((?!\.global).)*\.scss$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true
+              }
+            },
+            { loader: 'sass-loader' }
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig, ExtractTextPluginConfig]
+};

b18ee2ad63b7326fd72586b71883b3bda5fc3ad9
mtretiak <mtretiak@corelogic.com>
2018-05-24 19:17:55 +0300

    add animation for message appearance

--- src/containers/MessageList/MessageList.js
+++ src/containers/MessageList/MessageList.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 import Message from '../../components/Message/Message';
 
+import './messageList.global.scss';
 import styles from './messageList.scss';
 
 export default class MessageList extends Component {
@@ -10,10 +12,18 @@ export default class MessageList extends Component {
 
     return (
       <div className={styles.list}>
+        <CSSTransitionGroup
+            transitionName="messages"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={300}
+            transitionAppear={true}
+            transitionAppearTimeout={500}
+        >
         {messages.map(message => <Message
             key={message.id}
             updateChat={updateChat}
             {...message} />)}
+        </CSSTransitionGroup>
       </div>
     );
   }
